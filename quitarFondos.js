6dc3e68f4c3b4c1c6cb08624aca0675cdb5c6fad
DPooL2004 <dsrp002@gmail.com>
2024-03-19 20:09:35 -0500

    cambio nombre del archivo y termino funciones de quitar fondo

---
+++ quitarFondos.js
@@ -0,0 +1,85 @@
+let card = document.getElementById('Card')
+let card2 = document.getElementById('Card2')
+let card3 = document.getElementById('Card3')
+let card4 = document.getElementById('Card4')
+let card5 = document.getElementById('Card5')
+let card6 = document.getElementById('Card6')
+let card7 = document.getElementById('Card7')
+let card8 = document.getElementById('Card8')
+let card9 = document.getElementById('Card9')
+let card10 = document.getElementById('Card10')
+let card11 = document.getElementById('Card11')
+let card12 = document.getElementById('Card12')
+let card13 = document.getElementById('Card13')
+let card14 = document.getElementById('Card14')
+let card15 = document.getElementById('Card15')
+let card16 = document.getElementById('Card16')
+
+
+card.addEventListener('click', quitarFondo)
+card2.addEventListener('click', quitarFondo2)
+card3.addEventListener('click', quitarFondo3)
+card4.addEventListener('click', quitarFondo4)
+card5.addEventListener('click', quitarFondo5)
+card6.addEventListener('click', quitarFondo6)
+card7.addEventListener('click', quitarFondo7)
+card8.addEventListener('click', quitarFondo8)
+card9.addEventListener('click', quitarFondo9)
+card10.addEventListener('click', quitarFondo10)
+card11.addEventListener('click', quitarFondo11)
+card12.addEventListener('click', quitarFondo12)
+card13.addEventListener('click', quitarFondo13)
+card14.addEventListener('click', quitarFondo14)
+card15.addEventListener('click', quitarFondo15)
+card16.addEventListener('click', quitarFondo16)
+
+
+
+function quitarFondo(){
+    document.getElementById('imagen').setAttribute("src", " ")
+}
+function quitarFondo2(){
+    document.getElementById('imagen2').setAttribute("src", " ")
+}
+function quitarFondo3(){
+    document.getElementById('imagen3').setAttribute("src", " ")
+}
+function quitarFondo4(){
+    document.getElementById('imagen4').setAttribute("src", " ")
+}
+function quitarFondo5(){
+    document.getElementById('imagen5').setAttribute("src", " ")
+}
+function quitarFondo6(){
+    document.getElementById('imagen6').setAttribute("src", " ")
+}
+function quitarFondo7(){
+    document.getElementById('imagen7').setAttribute("src", " ")
+}
+function quitarFondo8(){
+    document.getElementById('imagen8').setAttribute("src", " ")
+}
+function quitarFondo9(){
+    document.getElementById('imagen9').setAttribute("src", " ")
+}
+function quitarFondo10(){
+    document.getElementById('imagen10').setAttribute("src", " ")
+}
+function quitarFondo11(){
+    document.getElementById('imagen11').setAttribute("src", " ")
+}
+function quitarFondo12(){
+    document.getElementById('imagen12').setAttribute("src", " ")
+}
+function quitarFondo13(){
+    document.getElementById('imagen13').setAttribute("src", " ")
+}
+function quitarFondo14(){
+    document.getElementById('imagen14').setAttribute("src", " ")
+}
+function quitarFondo15(){
+    document.getElementById('imagen15').setAttribute("src", " ")
+}
+function quitarFondo16(){
+    document.getElementById('imagen16').setAttribute("src", " ")
+}
