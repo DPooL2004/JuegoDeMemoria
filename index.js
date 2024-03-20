287b8231185841d0d00c27ab92fd1f6a8ffef944
sofiaTrujilloGit <mariasofiatrujillomuza@gmail.com>
2024-03-20 10:08:00 -0500

    se remueve archivo ponerFondos se cambia nombre del js y se actualiza etiqueta script en html

---
+++ index.js
@@ -0,0 +1,150 @@
+let boton = document.getElementById('botonJugar')
+
+let card = document.getElementById('Card')
+let card2 = document.getElementById('Card2')
+let card3 = document.getElementById('Card3')
+let card4 = document.getElementById('Card4')
+let card5 = document.getElementById('Card5')
+let card6 = document.getElementById('Card6')
+let card7 = document.getElementById('Card7')
+let card8 = document.getElementById('Card8')
+let card9 = document.getElementById('Card9')
+let card10 = document.getElementById('Card10')
+let card11 = document.getElementById('Card11')
+let card12 = document.getElementById('Card12')
+let card13 = document.getElementById('Card13')
+let card14 = document.getElementById('Card14')
+let card15 = document.getElementById('Card15')
+let card16 = document.getElementById('Card16')
+
+let imagen = document.getElementById('imagen')
+let imagen2 = document.getElementById('imagen2')
+let imagen3 = document.getElementById('imagen3')
+let imagen4 = document.getElementById('imagen4')
+let imagen5 = document.getElementById('imagen5')
+let imagen6 = document.getElementById('imagen6')
+let imagen7 = document.getElementById('imagen7')
+let imagen8 = document.getElementById('imagen8')
+let imagen9 = document.getElementById('imagen9')
+let imagen10 = document.getElementById('imagen10')
+let imagen11 = document.getElementById('imagen11')
+let imagen12 = document.getElementById('imagen12')
+let imagen13 = document.getElementById('imagen13')
+let imagen14 = document.getElementById('imagen14')
+let imagen15 = document.getElementById('imagen15')
+let imagen16 = document.getElementById('imagen16')
+
+boton.addEventListener('click', animaciones)
+
+function animaciones(){
+    imagen.classList.toggle("respaldo")
+    imagen2.classList.toggle('respaldo')
+    imagen3.classList.toggle("respaldo")
+    imagen4.classList.toggle('respaldo')
+    imagen5.classList.toggle("respaldo")
+    imagen6.classList.toggle('respaldo')
+    imagen7.classList.toggle("respaldo")
+    imagen8.classList.toggle('respaldo')
+    imagen9.classList.toggle("respaldo")
+    imagen10.classList.toggle('respaldo')
+    imagen11.classList.toggle("respaldo")
+    imagen12.classList.toggle('respaldo')
+    imagen13.classList.toggle("respaldo")
+    imagen14.classList.toggle('respaldo')
+    imagen15.classList.toggle("respaldo")
+    imagen16.classList.toggle("respaldo")
+
+    card.addEventListener('click', quitarFondo)
+    card2.addEventListener('click', quitarFondo2)
+    card3.addEventListener('click', quitarFondo3)
+    card4.addEventListener('click', quitarFondo4)
+    card5.addEventListener('click', quitarFondo5)
+    card6.addEventListener('click', quitarFondo6)
+    card7.addEventListener('click', quitarFondo7)
+    card8.addEventListener('click', quitarFondo8)
+    card9.addEventListener('click', quitarFondo9)
+    card10.addEventListener('click', quitarFondo10)
+    card11.addEventListener('click', quitarFondo11)
+    card12.addEventListener('click', quitarFondo12)
+    card13.addEventListener('click', quitarFondo13)
+    card14.addEventListener('click', quitarFondo14)
+    card15.addEventListener('click', quitarFondo15)
+    card16.addEventListener('click', quitarFondo16)
+
+    function quitarFondo(){
+        imagen.classList.toggle("back2")
+    }
+    function quitarFondo2(){
+        imagen2.classList.toggle('back3')
+    }
+    function quitarFondo3(){
+        imagen3.classList.toggle("back2")
+    }
+    function quitarFondo4(){
+        imagen4.classList.toggle('back3')
+    }
+    function quitarFondo5(){
+        imagen5.classList.toggle("back2")
+    }
+    function quitarFondo6(){
+        imagen6.classList.toggle('back3')
+    }
+    function quitarFondo7(){
+        imagen7.classList.toggle("back3")
+    }
+    function quitarFondo8(){
+        imagen8.classList.toggle('back2')
+    }
+    function quitarFondo9(){
+        imagen9.classList.toggle("back3")
+    }
+    function quitarFondo10(){
+    
+        imagen10.classList.toggle('back2')
+    }
+    function quitarFondo11(){
+    
+        imagen11.classList.toggle("back3")
+    }
+    function quitarFondo12(){
+    
+        imagen12.classList.toggle('back2')
+    }
+    function quitarFondo13(){
+    
+        imagen13.classList.toggle("back3")
+    }
+    function quitarFondo14(){
+    
+        imagen14.classList.toggle('back2')
+    }
+    function quitarFondo15(){
+    
+        imagen15.classList.toggle("back3")
+    }
+    function quitarFondo16(){
+    
+        imagen16.classList.toggle('back2')
+    }
+
+    let tiempo = 60
+    let temporizador = document.getElementById('temporizador')
+    let time = false;
+    let finTime = null
+
+    if(time==false){
+        tiempoJuego();
+        time = true;
+    }
+
+    function tiempoJuego(){
+        finTime = setInterval(()=>{
+            tiempo--;
+            temporizador.innerHTML = ` ${tiempo} segundos`;
+            if (tiempo==0){
+                clearInterval(finTime);
+                alert('Se acabo el tiempo!')
+            }
+        },1000)
+    }
+}
