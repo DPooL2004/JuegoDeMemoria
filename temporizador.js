cb937e6e018fef2b61ef2335dc543fc2c6714744
sofiaTrujilloGit <mariasofiatrujillomuza@gmail.com>
2024-03-20 09:09:44 -0500

    documento js del temporizador

---
+++ temporizador.js
@@ -0,0 +1,22 @@
+let tiempo = 5
+let temporizador = document.getElementById('temporizador')
+let time = false;
+let finTime = null
+
+if(time==false){
+    tiempoJuego();
+    time = true;
+}
+
+function tiempoJuego(){
+    finTime = setInterval(()=>{
+        tiempo--;
+        temporizador.innerHTML = ` ${tiempo} segundos`;
+        if (tiempo==0){
+            clearInterval(finTime);
+            alert('Se acabo el tiempo!')
+        }
+    },1000)
+}
+
+
